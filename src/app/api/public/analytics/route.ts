5b6959e9a4c0a845f099d6a328dbb212985cd18c
Alfonsus Ardani <alfonsusac@gmail.com>
2025-03-14 14:13:37 +0700

    - wip umami, using prisma singleton

--- src/app/api/public/analytics/route.ts
+++ src/app/api/public/analytics/route.ts
@@ -1,5 +1,5 @@
-import type { NextRequest } from "next/server";
-import { PrismaClient } from '@prisma/client'
+import { after, type NextRequest } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export async function POST(r: NextRequest) {
   const location = r.headers.get('x-vercel-ip-country') ?? 'unknown'
@@ -22,63 +22,86 @@ export async function POST(r: NextRequest) {
 
   const dataset_name = "main-app-dataset";
 
-  // Axiom
-  await fetch(`https://api.axiom.co/v1/datasets/${ dataset_name }/ingest`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${ process.env.AXIOM_API_KEY }`
-    },
-    body: JSON.stringify([{
-      level: 'info',
-      ...data,
-    }])
+  after(async () => {
+    // Axiom
+    await fetch(`https://api.axiom.co/v1/datasets/${ dataset_name }/ingest`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${ process.env.AXIOM_API_KEY }`
+      },
+      body: JSON.stringify([{
+        level: 'info',
+        ...data,
+      }])
+    })
+    // console.log("Logged to Axiom")
   })
-  console.log("Logged to Axiom")
 
-  // Umami
-  await fetch('https://analytics.arinji.com/api/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${ process.env.UMAMI_API_KEY }`
-    },
-    body: JSON.stringify({
-      type: 'event',
-      payload: {
+
+  // // Umami
+  // const ures = await fetch('https://analytics.arinji.com/api/send', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Authorization': `Bearer ${ process.env.UMAMI_TOKEN }`,
+  //     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+  //   },
+  //   body: JSON.stringify({
+  //     type: 'event',
+  //     payload: {
+  //       // // hostname: '',
+  //       // // language: 'en-US',
+  //       // // referer,
+  //       // // screen: '1920x1080',
+  //       // // title: project,
+  //       // // url: '',
+  //       // "hostname": "your-hostname",
+  //       // "language": "en-US",
+  //       // "referrer": "",
+  //       // "screen": "1920x1080",
+  //       // "title": "dashboard",
+  //       // "url": "/",
+  //       website: process.env.UMAMI_WEBSITE_ID,
+  //       // "name": "event-name",
+  //       // "data": {
+  //       //   "foo": "bar"
+  //       // }
+  //       // name: event,
+  //       // data: {
+  //       //   location,
+  //       //   meta,
+  //       // }
+  //     }
+  //   })
+  // })
+  // console.log(ures)
+  // console.log(await ures.text())
+  // console.log("Logged to Umami")
+
+  after(async () => {
+    const pres = await prisma.thing.upsert({
+      where: {
+        id: project,
         referer,
-        title: project,
-        name: event,
-        websiteId: process.env.UMAMI_WEBSITE_ID,
-        data: {
-          location,
-          meta,
-        }
+        location,
+        event,
+      },
+      create: {
+        id: project,
+        referer,
+        location,
+        event,
+        count: 1,
+      },
+      update: {
+        count: { increment: 1 }
       }
     })
+    // console.log(pres)
+    // console.log("Logged to DB")
   })
-  console.log("Logged to Umami")
 
-  const prisma = new PrismaClient()
-  prisma.thing.upsert({
-    where: {
-      id: project,
-      referer,
-      location,
-      event,
-    },
-    create: {
-      id: project,
-      referer,
-      location,
-      event,
-      count: 1,
-    },
-    update: {
-      count: { increment: 1 }
-    }
-  })
-  console.log("Logged to DB")
 
   return new Response(null, {
     status: 204, // No Content
