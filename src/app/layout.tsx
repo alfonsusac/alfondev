53a651078baa16d95d1045e1ca6177db1d28f345
Alfonsus Ardani <alfonsusac@gmail.com>
2025-03-20 16:20:25 +0700

    addeed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script id="umami-script" defer src="https://analytics.arinji.com/script.js" data-website-id="d22e04da-dabc-40da-9ced-ddc457aa3ab9"></script>
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ geistSans.variable } ${ geistMono.variable } antialiased`}
       >
         {children}
       </body>
